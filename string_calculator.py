b770e528aeceebc874662eea627f73cf675e92a3
Aditya Bose <adi26mar@gmail.com>
2025-03-13 00:02:06 +0530

    update implementation to handle arbitrary \n as a delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,6 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        numbers = numbers.replace("\n", ",")
         # Split string of comma separated numbers and add them
         return sum(int(n) for n in numbers.split(","))
