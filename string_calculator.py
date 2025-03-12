74cfd54e1b5db2126b3a238f0922be1e4e50586d
Aditya Bose <adi26mar@gmail.com>
2025-03-13 00:52:22 +0530

    update implementation to handle string with a specified delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -2,6 +2,10 @@ class StringCalculator:
     def add(self, numbers):
         if numbers == "":
             return 0
+        if numbers.startswith("//"):
+            delimiter = numbers[2]
+            numbers = numbers[4:]  # Skip "//[delimiter]\n"
+            return sum(int(n) for n in numbers.split(delimiter))
         numbers = numbers.replace("\n", ",")
         # Split string of comma separated numbers and add them
         return sum(int(n) for n in numbers.split(","))
