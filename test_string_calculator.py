89542e924482cd213ede4a63266f42d121e269a3
Aditya Bose <adi26mar@gmail.com>
2025-03-13 00:43:13 +0530

    add failing test to support different delimiters

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -27,6 +27,11 @@ class TestStringCalculator(unittest.TestCase):
         result = calc.add("1\n2,3")
         self.assertEqual(result, 6)
 
+    def test_custom_delimiter(self):
+        calc = StringCalculator()
+        result = calc.add("//;\n1;2")
+        self.assertEqual(result, 3)
+
 
 if __name__ == "__main__":
     unittest.main()
